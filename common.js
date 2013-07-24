abe9c177af710b8dea5c3a25a2e3f21e67826d0c
NikhilGanage <nikeg14@gmail.com>
2013-07-24 13:27:52 +0530

    Delete common.js

--- common.js
+++
@@ -1,6 +0,0 @@
-function onDeviceReady(){
-  document.getElementById("helloworld").innerHtml = "Hello World! Loaded PhoneGap Framework!"
-}
-function init(){
-	document.addEventListener("deviceready",onDeviceReady,false);
-}
